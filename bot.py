c484dd17c2a0f4c588b542a13007665d9a75ff75
wethegreenpeople <superjohn007@gmail.com>
2015-11-11 22:49:38 -0800

    Cleaning code
    Cleaning up some code, there was some redundant code.
    
    Also almost have the stats fixed. Cycling through the dictionary to find the stats I want is working pretty well.

--- bot.py
+++ bot.py
@@ -22,8 +22,12 @@ def on_message(message):
     if message.content.startswith('!hello'):
         client.send_message(message.channel, 'Hello {}! How are you?'.format(message.author.name))
 
-    if message.content.startswith("!flood"):
-        client.send_message(message.channel, "\n" * 50)
+    admins = ["87250476927549441", "87250476927549440"]
+    if message.content.startswith("?flood"):
+        if message.author.id in admins:
+            client.send_message(message.channel, "\n" * 50)
+        else:
+            client.send_message(message.channel, message.author.id)
 
     if message.content.startswith("!joke"):
         content = requests.get("http://tambal.azurewebsites.net/joke/random")
@@ -38,20 +42,70 @@ def on_message(message):
     if message.content.startswith("!stats"):
         lolStats(message)
 
+    if message.content.startswith("!teststats"):
+        statsTest(message)
+
     if message.content.startswith("!id"):
         lolId(message)
 
     if message.content.startswith("!matchhistory"):
         lolMatchhistory(message)
+        
+    if message.content.startswith("!eumatchhistory"):
+        lolMatchhistoryEU(message)
 
     if message.content.startswith("!freechamps"):
         lolFreeChampions(message)
 
     if message.content.startswith("!help"):
-        helpFile = open("/home/john/Desktop/Programming/Python/help.txt")
+        helpFile = open("help.txt")
         fileContent = helpFile.read()
-        client.send_message(message.channel, fileContent)
+        client.send_message(message.author, fileContent)
         helpFile.close()
+
+def statsTest(message):
+   #removes alerting code, spaces, and makes summoner name lower case
+    summoner = message.content.replace("!teststats ", "")
+    summoner = summoner.replace(" ", "")
+    summoner = summoner.lower()
+
+    #checks to see if the value inputted is a number or not. So you don't have to run two api calls needlessly
+    try:
+        int(summoner)
+    except ValueError:
+        #finding the ID from the username
+        leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+summoner+"?<API KEY>")
+        if leaguePull.status_code == 200:
+            summoner = str(leaguePull.json()[summoner]["id"])
+        else:
+            client.send_message(message.channel, leaguePull.status_code)
+
+    #pulling the stats
+    leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/"+summoner+"/summary?season=SEASON2015&<API KEY>")
+    aNumber = 0
+    anotherNumber = 0
+    unrankedWins = []
+    unrankedMinions = []
+    unrankedChampions = []
+    client.send_message(message.channel, str(leaguePull.json()["playerStatSummaries"][aNumber]["playerStatSummaryType"]))
+    while str(leaguePull.json()["playerStatSummaries"][aNumber]["playerStatSummaryType"]) != "Unranked":
+        aNumber += 1
+        anotherNumber += 1
+
+    unrankedWins.append(str(leaguePull.json()["playerStatSummaries"][anotherNumber]["wins"]))
+    unrankedMinions.append(str(leaguePull.json()["playerStatSummaries"][anotherNumber]["aggregatedStats"]["totalMinionKills"]))
+    unrankedChampions.append(str(leaguePull.json()["playerStatSummaries"][anotherNumber]["aggregatedStats"]["totalChampionKills"]))
+
+    if leaguePull.status_code == 200:
+        client.send_message(message.channel,
+            "**Unranked**\n\n" + 
+            "```" +
+            "Minion Kills: " + str(unrankedMinions) + 
+            "\nChampion Kills: " + str(unrankedChampions) +
+            "\nWins: " + str(unrankedWins) +
+            "```")
+    else:
+        client.send_message(message.channel, leaguePull.status_code)
 
 def lolStats(message):
     #Removes !lol from the setence
@@ -110,128 +164,198 @@ def lolStats(message):
         client.send_message(message.channel, leaguePull.status_code)
 
 def lolId(message):
-    #removes !id from the sentence
-    item = [message.content]
-    words = ["!id "]
-    item1 = []
-    for line in item:
-        for w in words:
-            line = line.replace(w, "")
-        item1.append(line)
+    #removes alerting code, spaces, and makes summoner name lower case
+    summoner = message.content.replace("!id ", "")
+    summoner = summoner.replace(" ", "")
+    summoner = summoner.lower()
+
     #pulls the summoners ID from his username
-    leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+line+"?<API KEY>")
+    leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+summoner+"?<API KEY>")
     if leaguePull.status_code == 200:
         client.send_message(message.channel, "Summoner ID\n\n" + "```" +
-            str(leaguePull.json()[line]["id"]) + 
+            str(leaguePull.json()[summoner]["id"]) + 
             "```")
     else:
         client.send_message(message.channel, leaguePull.status_code)
 
 def lolMatchhistory(message):
-    #removes !matchhistory from the sentence
-    item = [message.content]
-    words = ["!matchhistory "]
-    item1 = []
-    for line in item:
-        for w in words:
-            line = line.replace(w, "")
-        item1.append(line)
-    #checks to see if the value inputted is a number or not. So you don't have to run two api calls needlessly
-    try:
-        int(line)
-    except ValueError:
+   #removes alerting code, spaces, and makes summoner name lower case
+    summoner = message.content.replace("!matchhistory ", "")
+    summoner = summoner.lower()
+    listCheck = summoner.split()
+    summoner = str(listCheck[0])
+    if "eu" in listCheck or "euw" in listCheck:
         #finding the ID from the username
-        leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+line+"?<API KEY>")
+        leaguePull = requests.get("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/"+summoner+"?<API KEY>")
         if leaguePull.status_code == 200:
-            line = str(leaguePull.json()[line]["id"])
+            summoner = str(leaguePull.json()[summoner]["id"])
         else:
             client.send_message(message.channel, leaguePull.status_code)
+        #pulling the game stats from the last game
+        leaguePull = requests.get("https://euw.api.pvp.net/api/lol/euw/v1.3/game/by-summoner/"+summoner+"/recent?<API KEY>")
+        if leaguePull.status_code == 200:
+            if leaguePull.json()["games"][0]["stats"]["win"] == True:
+                winStatus = "****Game Won!****"
+            else:
+                winStatus = "****Game Lost****"
+            
+            gameLength = (int(leaguePull.json()["games"][0]["stats"]["timePlayed"]) / 60)
+            #pulls champipn name from it's ID
+            championPull = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+str(leaguePull.json()["games"][0]["championId"])+"?champData=info,recommended&<API KEY>")
+            championUsed = str(championPull.json()["name"])
 
-    #pulling the game stats from the last game
-    leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/"+line+"/recent?<API KEY>")
-    if leaguePull.status_code == 200:
-        if leaguePull.json()["games"][0]["stats"]["win"] == True:
-            winStatus = "****Game Won!****"
+            try: 
+                numDeaths = str(leaguePull.json()["games"][0]["stats"]["numDeaths"])
+            except KeyError:
+                numDeaths = str("0")
+            
+            try: 
+                champsKilled = str(leaguePull.json()["games"][0]["stats"]["championsKilled"])
+            except KeyError:
+                champsKilled = str("0")
+
+            try: 
+                assists = str(leaguePull.json()["games"][0]["stats"]["assists"])
+            except KeyError:
+                assists = str("0")
+
+            client.send_message(message.channel,
+                "**Latest Match History**\n\n" + 
+                "```" +
+                winStatus +
+                "\nNumber of deaths: " + numDeaths +
+                "\nChampions killed: " + champsKilled +
+                "\nAssists: " + assists +
+                "\nLength of game: " + str(gameLength) + " minutes" +
+                "\nChampion used: " + championUsed +
+                "```")
+
+            #pulling stats from the second to last game played
+            if leaguePull.json()["games"][1]["stats"]["win"] == True:
+                winStatus = "****Game Won!****"
+            else:
+                winStatus = "****Game Lost****"
+            gameLength = (int(leaguePull.json()["games"][1]["stats"]["timePlayed"]) / 60)
+            championPull = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+str(leaguePull.json()["games"][1]["championId"])+"?champData=info,recommended&<API KEY>")
+            championUsed = str(championPull.json()["name"])
+
+            try: 
+                str(leaguePull.json()["games"][1]["stats"]["numDeaths"])
+                numDeaths = str(leaguePull.json()["games"][1]["stats"]["numDeaths"])
+            except KeyError:
+                numDeaths = str("0")
+
+            try: 
+                str(leaguePull.json()["games"][1]["stats"]["championsKilled"])
+                champsKilled = str(leaguePull.json()["games"][1]["stats"]["championsKilled"])
+            except KeyError:
+                champsKilled = str("0")
+
+            try: 
+                str(leaguePull.json()["games"][1]["stats"]["assists"])
+                assists = str(leaguePull.json()["games"][1]["stats"]["assists"])
+            except KeyError:
+                assists = str("0")
+
+            client.send_message(message.channel,
+                "**Previous Match History**\n\n" + 
+                "```" +
+                winStatus +
+                "\nNumber of deaths: " + numDeaths +
+                "\nChampions killed: " + champsKilled +
+                "\nAssists: " + assists +
+                "\nLength of game: " + str(gameLength) + " minutes" +
+                "\nChampion used: " + championUsed +
+                "```")
+
+    else:
+        #finding the ID from the username
+        leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/"+summoner+"?<API KEY>")
+        if leaguePull.status_code == 200:
+            summoner = str(leaguePull.json()[summoner]["id"])
         else:
-            winStatus = "****Game Lost****"
-        
-        gameLength = (int(leaguePull.json()["games"][0]["stats"]["timePlayed"]) / 60)
-        #pulls champipn name from it's ID
-        championPull = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+str(leaguePull.json()["games"][0]["championId"])+"?champData=info,recommended&<API KEY>")
-        championUsed = str(championPull.json()["name"])
+            client.send_message(message.channel, leaguePull.status_code)
+        #pulling the game stats from the last game
+        leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/"+summoner+"/recent?<API KEY>")
+        if leaguePull.status_code == 200:
+            if leaguePull.json()["games"][0]["stats"]["win"] == True:
+                winStatus = "****Game Won!****"
+            else:
+                winStatus = "****Game Lost****"
+            
+            gameLength = (int(leaguePull.json()["games"][0]["stats"]["timePlayed"]) / 60)
+            #pulls champipn name from it's ID
+            championPull = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+str(leaguePull.json()["games"][0]["championId"])+"?champData=info,recommended&<API KEY>")
+            championUsed = str(championPull.json()["name"])
 
-        try: 
-            numDeaths = str(leaguePull.json()["games"][0]["stats"]["numDeaths"])
-        except KeyError:
-            numDeaths = str("0")
-        
-        try: 
-            champsKilled = str(leaguePull.json()["games"][0]["stats"]["championsKilled"])
-        except KeyError:
-            champsKilled = str("0")
+            try: 
+                numDeaths = str(leaguePull.json()["games"][0]["stats"]["numDeaths"])
+            except KeyError:
+                numDeaths = str("0")
+            
+            try: 
+                champsKilled = str(leaguePull.json()["games"][0]["stats"]["championsKilled"])
+            except KeyError:
+                champsKilled = str("0")
 
-        try: 
-            assists = str(leaguePull.json()["games"][0]["stats"]["assists"])
-        except KeyError:
-            assists = str("0")
+            try: 
+                assists = str(leaguePull.json()["games"][0]["stats"]["assists"])
+            except KeyError:
+                assists = str("0")
 
-        client.send_message(message.channel,
-            "**Latest Match History**\n\n" + 
-            "```" +
-            winStatus +
-            "\nNumber of deaths: " + numDeaths +
-            "\nChampions killed: " + champsKilled +
-            "\nAssists: " + assists +
-            "\nLength of game: " + str(gameLength) + " minutes" +
-            "\nChampion used: " + championUsed +
-            "```")
+            client.send_message(message.channel,
+                "**Latest Match History**\n\n" + 
+                "```" +
+                winStatus +
+                "\nNumber of deaths: " + numDeaths +
+                "\nChampions killed: " + champsKilled +
+                "\nAssists: " + assists +
+                "\nLength of game: " + str(gameLength) + " minutes" +
+                "\nChampion used: " + championUsed +
+                "```")
 
-        #pulling stats from the second to last game played
-        if leaguePull.json()["games"][1]["stats"]["win"] == True:
-            winStatus = "****Game Won!****"
-        else:
-            winStatus = "****Game Lost****"
-        gameLength = (int(leaguePull.json()["games"][1]["stats"]["timePlayed"]) / 60)
-        championPull = requests.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/"+str(leaguePull.json()["games"][1]["championId"])+"?champData=info,recommended&<API KEY>")
-        championUsed = str(championPull.json()["name"])
+            #pulling stats from the second to last game played
+            if leaguePull.json()["games"][1]["stats"]["win"] == True:
+                winStatus = "****Game Won!****"
+            else:
+                winStatus = "****Game Lost****"
+            gameLength = (int(leaguePull.json()["games"][1]["stats"]["timePlayed"]) / 60)
+            championPull = requests.get("https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion/"+str(leaguePull.json()["games"][1]["championId"])+"?champData=info,recommended&<API KEY>")
+            championUsed = str(championPull.json()["name"])
 
-        try: 
-            str(leaguePull.json()["games"][1]["stats"]["numDeaths"])
-            numDeaths = str(leaguePull.json()["games"][1]["stats"]["numDeaths"])
-        except KeyError:
-            numDeaths = str("0")
+            try: 
+                str(leaguePull.json()["games"][1]["stats"]["numDeaths"])
+                numDeaths = str(leaguePull.json()["games"][1]["stats"]["numDeaths"])
+            except KeyError:
+                numDeaths = str("0")
 
-        try: 
-            str(leaguePull.json()["games"][1]["stats"]["championsKilled"])
-            champsKilled = str(leaguePull.json()["games"][1]["stats"]["championsKilled"])
-        except KeyError:
-            champsKilled = str("0")
+            try: 
+                str(leaguePull.json()["games"][1]["stats"]["championsKilled"])
+                champsKilled = str(leaguePull.json()["games"][1]["stats"]["championsKilled"])
+            except KeyError:
+                champsKilled = str("0")
 
-        try: 
-            str(leaguePull.json()["games"][1]["stats"]["assists"])
-            assists = str(leaguePull.json()["games"][1]["stats"]["assists"])
-        except KeyError:
-            assists = str("0")
+            try: 
+                str(leaguePull.json()["games"][1]["stats"]["assists"])
+                assists = str(leaguePull.json()["games"][1]["stats"]["assists"])
+            except KeyError:
+                assists = str("0")
 
-        client.send_message(message.channel,
-            "**Previous Match History**\n\n" + 
-            "```" +
-            winStatus +
-            "\nNumber of deaths: " + numDeaths +
-            "\nChampions killed: " + champsKilled +
-            "\nAssists: " + assists +
-            "\nLength of game: " + str(gameLength) + " minutes" +
-            "\nChampion used: " + championUsed +
-            "```")
+            client.send_message(message.channel,
+                "**Previous Match History**\n\n" + 
+                "```" +
+                winStatus +
+                "\nNumber of deaths: " + numDeaths +
+                "\nChampions killed: " + champsKilled +
+                "\nAssists: " + assists +
+                "\nLength of game: " + str(gameLength) + " minutes" +
+                "\nChampion used: " + championUsed +
+                "```")
+
+def lolMatchhistoryEU(message):
+    client.send_message(message.channel, "Command changed. Please use !matchhistory <summoner name> <region> instead. Use !help for more info")
 
 def lolFreeChampions(message):
-    item = [message.content]
-    words = ["!freechamps "]
-    item1 = []
-    for line in item:
-        for w in words:
-            line = line.replace(w, "")
-        item1.append(line)
     #pulls free to play champs
     leaguePull = requests.get("https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&<API KEY>")
     #pulls champs info, so I can convert an ID to a name
@@ -253,9 +377,6 @@ def lolFreeChampions(message):
             )
     else: 
         client.send_message(message.channel, leaguePull.status_code)
-
-
-
 
 
 client.run()
